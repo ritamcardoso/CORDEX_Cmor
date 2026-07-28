6745483576087c328446ffab41890f6a0aabf1f5
ritamcardoso <wrf@vortex.degge.fc.ul.pt>
2026-07-29 00:46:23 +0100

    Force updated files from HPC

--- scripts/run_out_generic.sh
+++ scripts/run_out_generic.sh
@@ -146,7 +146,7 @@ for(( j = ${yeari}; j <= ${yearf}; j++ )) ; do
 #
 #  Create list from header_d0?.ini + header_[var]
 #
-    cat ${HEADER_INI_DIR}/${exp}_${ini_kind}${dom_id}_${run[$r]}.ini | sed \ 
+    cat ${HEADER_INI_DIR}/${exp}_${ini_kind}${dom_id}_${run[$r]}.ini | sed \
       -e "s|_START_YY_|$START_YY|g" \
       -e "s|_END_YY_|$END_YY|g" \
       -e "s|_OUTPUT_WRF_|${OUTPUT_WRF}/|g" \
